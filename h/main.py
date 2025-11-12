500732518eb1cde797f3f5592e2fa97b06922bf5
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-11-12 08:32:31 -0500

    ejemp_python
    ejemp_python

---
+++ h/main.py
@@ -0,0 +1,9 @@
+# Se asigna el valor 50 a la variable 'width' (ancho)
+width = 50
+
+# Se calcula 45 * 8 y el resultado (360) se asigna a la variable 'height' (alto)
+height = 45 * 8
+
+# Se multiplica 'width' por 'height' (50 * 360 = 18000)
+# Luego se imprime el resultado en pantalla
+print(width * height)
